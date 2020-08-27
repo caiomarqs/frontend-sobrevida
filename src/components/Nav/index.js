51954d1c3ead25286d0b7df6d5125067ca2c0b0f
caiomarqs <caiomarques1412@gmail.com>
2020-08-27 12:19:00 -0300

    Add constants

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import constants from '../../constants/texts'
 import './index.css'
 
 const Nav = () => {
     return (
         <header id="nav">
-
+            <h1>{constants["nav.titulo"]}</h1>
         </header>
     )
 }
---
+++ src/constants/texts.js
@@ -0,0 +1,3 @@
+export default {
+    'nav.titulo': 'sobreVida'
+}
