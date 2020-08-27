886a004b2441dd936639746839d0545738848b45
caiomarqs <caiomarques1412@gmail.com>
2020-08-27 12:12:46 -0300

    Add nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './index.css'
+
+const Nav = () => {
+    return (
+        <header id="nav">
+
+        </header>
+    )
+}
+
+export { Nav };
--- src/view/Home.js
+++ src/view/Home.js
@@ -2,14 +2,9 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-<<<<<<< HEAD
-            <h1>teste deu errado</h1>
-=======
-            <h1>teste deu qlafkcd</h1>
->>>>>>> ddc495e16db59393cbd864ce97d958d281f79c33
-        </div>
+        <>
+
+        </>
     )
 }
 
