ca80e2a0a9d23b4a89bb3cea43881acee24c04a6
guiparussolo <guiparussolo@gmail.com>
2020-08-27 12:22:17 -0300

    reset repo

--- src/view/Home.js
+++ src/view/Home.js
@@ -4,11 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-<<<<<<< HEAD
             <h1>teste deu errado</h1>
-=======
-            <h1>teste deu qlafkcd</h1>
->>>>>>> ddc495e16db59393cbd864ce97d958d281f79c33
         </div>
     )
 }
