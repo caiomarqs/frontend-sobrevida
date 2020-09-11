0ffa8854b0712e92088ffa3a37e53bc80e520403
caiomarqs <caiomarques1412@gmail.com>
2020-09-11 12:47:15 -0300

    Remove env var print

--- src/services/Watson.js
+++ src/services/Watson.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const sendQuestion = (question) => {
-    console.log(process.env.REACT_APP_SERVER_URL)
     return axios({
         baseURL: process.env.REACT_APP_SERVER_URL,
         method: 'GET', 
